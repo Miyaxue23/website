6a341498cffef7802ad0f975d7baa5c446e32101
Miyaxue23 <Miyaxue23@outlook.com>
2024-02-26 11:43:07 +0800

    chore: update demo

--- src/views/Index-page.vue
+++ src/views/Index-page.vue
@@ -6,9 +6,15 @@
         A new social model, an innovative co-building model, profitable contributions, and a
         tradable hive.
       </div>
-      <div class="al-c create-btn" @click="router.push('/create')">
-        <img src="../assets/img/create.svg" width="32" alt="" />
-        <span class="ml-2 fw-b"> Create A Hive </span>
+      <div class="al-c mt-8">
+        <div class="al-c create-btn" @click="router.push('/create')">
+          <img src="../assets/img/create.svg" width="32" alt="" />
+          <span class="ml-2 fw-b"> Create A Hive </span>
+        </div>
+        <div class="al-c demo-btn ml-4" @click="handleDemo">
+          <img src="../assets/img/demo.svg" width="32" alt="" />
+          <span class="ml-2 fw-b">Demo</span>
+        </div>
       </div>
     </div>
   </div>
@@ -17,6 +23,10 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 const router = useRouter()
+
+const handleDemo = () => {
+  window.open('https://honey.4everland.app/#/demo-space/home')
+}
 </script>
 
 <style scoped lang="scss">
@@ -42,11 +52,16 @@ const router = useRouter()
     }
     .create-btn {
       display: inline-flex;
-      margin-top: 32px;
       padding: 16px 24px;
       box-sizing: border-box;
       border-radius: 8px;
       background: #f9cc45;
+      cursor: pointer;
+    }
+    .demo-btn {
+      border-radius: 8px;
+      border: 1px solid #f9cc45;
+      padding: 16px 24px;
       cursor: pointer;
     }
   }
